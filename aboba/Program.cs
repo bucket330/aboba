630c259b591615bbaa388e55e236ed31a2258ff6
bucket330 <bucketirn@gmail.com>
2023-02-27 17:28:31 +0000

    add vs project

---
+++ aboba/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+int[] arr = new int[64 * 1024 * 1024];
+
+for (int x = 1; x < 65; x <<= 1)
+{
+    Stopwatch sw = new Stopwatch();
+    sw.Start();
+    for (int i = 0; i < arr.Length; i += x) arr[i] *= 3;
+    sw.Stop();
+    Console.WriteLine(sw.ElapsedMilliseconds);
+}
